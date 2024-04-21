6c171971e892af2cc8a5f4dc439947f62163172e
⧉⧉⧉ <marek.miller@protonmail.com>
2024-04-21 17:42:37 +0200

    Add unit test for strlen

--- test/test_unit.s
+++ test/test_unit.s
@@ -1,22 +1,50 @@
 default rel
 
-%include "syscalls.inc"
 %include "malib.inc"
+%include "syscalls.inc"
 
 global _start
 
+test_fail		equ	EXIT_FAILURE
+
 SECTION .data
 
-exit_code		db	EXIT_SUCCESS
+str01		db	0
+str01_len 	equ	0
+str02		db	"This is a test", 0
+str02_len	equ	$-str02 -1
 
 SECTION .bss
 
 SECTION .text
+	extern ma_strlen, ma_printstr
 
 _start:
+	mov	rax, EXIT_SUCCESS
+	call	test_strlen
 
 _exit:
+	push	rax
 	mov	rax, SYS_EXIT
-	mov	rdi, [exit_code]
+	pop	rdi
 	syscall
+
+test_strlen:
+
+%macro case_strlen 2
+	push 	rax
+	mov	rdi, %1
+	call	ma_strlen
+
+	mov	rdx, %2
+	cmp	rax, rdx
+	je	%%end
+	mov	qword [rsp], test_fail
+%%end:	pop	rax
+%endmacro
+
+	case_strlen str01, str01_len
+	case_strlen str02, str02_len
+
+	ret
 
