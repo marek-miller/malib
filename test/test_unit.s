a20f41f957bb622e93d3e6a0cc3a8fb6c24ff8ba
Marek Miller <mlm@math.ku.dk>
2024-04-22 14:38:42 +0200

    Add test fail message

--- test/test_unit.s
+++ test/test_unit.s
@@ -5,9 +5,11 @@ default rel
 
 global _start
 
-test_fail		equ	EXIT_FAILURE
-
 SECTION .data
+
+TEST_RT		db	EXIT_SUCCESS		; test return value
+TEST_FAILMSG	db	"FAIL ", __FILE__, ":0x____", 0xa, 0
+TEST_FAILOFF	equ	$-6			; placeholder for line number
 
 str01		db	0
 str01_len 	equ	0
@@ -20,44 +22,56 @@ str03_len	equ	1024*1024
 str03		resb	str03_len+1
 
 SECTION .text
-	extern ma_strlen, ma_printstr
+	extern ma_print, ma_strlen, ma_toa
 
 _start:
-	mov	rax, EXIT_SUCCESS
 	call	test_strlen
 
 _exit:
-	push	rax
 	mov	rax, SYS_EXIT
-	pop	rdi
+	lea	rsi, [TEST_RT]
+	mov	rdi, [rsi]
 	syscall
 
-test_strlen:
-	; initiate str03
-	lea	rdi, [str03]
-	xor	rcx, rcx
-.set_str:
-	mov	[rdi + rcx], byte 'A'
-	inc	rcx
-	cmp	rcx, str03_len
-	jne	.set_str
-	mov	[rdi + rcx], byte 0
+; Print debugging info and set global return value to EXIT_FAILURE.
+_test_fail:
+	mov	rsi, rdi
+	mov	rdx, 4
+	lea	rdi, [TEST_FAILOFF]
+	call	ma_toa
+	mov	rdi, STDERR
+	lea	rsi, [TEST_FAILMSG]
+	call	ma_print
+	lea	rdi, [TEST_RT]
+        mov	[rdi], byte EXIT_FAILURE
+	ret
 
+test_strlen:
 	; check if ma_strlen returns correct length
 %macro case_strlen 2
-	push 	rax
 	lea	rdi, [%1]
 	call	ma_strlen
-
 	mov	rdx, %2
 	cmp	rax, rdx
 	je	%%.rt
-	mov	qword [rsp], test_fail
-%%.rt:	pop	rax
+	mov	rdi, __LINE__
+	call	_test_fail
+%%.rt:
 %endmacro
 
 	case_strlen str01, str01_len
 	case_strlen str02, str02_len
+
+	; initiate str03
+        	lea	rdi, [str03]
+        	xor	rcx, rcx
+        .set_str:
+        	mov	[rdi + rcx], byte 'A'
+        	inc	rcx
+        	cmp	rcx, str03_len
+        	jne	.set_str
+        	mov	[rdi + rcx], byte 0
+
 	case_strlen str03, str03_len
 
 	ret
