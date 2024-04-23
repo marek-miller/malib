3b796d3ff651078c7af94281c5f37fa3bb19f24b
⧉⧉⧉ <marek.miller@protonmail.com>
2024-04-23 18:45:26 +0200

    Rearrange ma_toa

--- src/malib.s
+++ src/malib.s
@@ -15,54 +15,44 @@ SECTION .text
 
 ma_print:
 	push	rdi			; calculate string length
-	push	rsi			; ma_srlnen need not preserve rdi/rsi
 	mov	rdi, rsi
-	call	ma_strlen
-	pop	rsi
+	call	ma_strlen		; we know that rsi is preserved here
 	pop	rdi
-
-	mov	rdx, rax		; rax = string length
+.l1:	mov	rdx, rax		; rax = string length
 	push	rax
 	mov	rax, SYS_WRITE
 	syscall
-
 .rt:	pop	rax
 	ret
-
 
 ma_strlen:
 	xor	rax, rax
 	mov	rcx, -1			; set the counter to max value
-	cld
 	repne 	scasb			; repeat until \0 character found
 	add	rcx, 2			; rcx = -#steps-2
 .rt:	sub	rax, rcx
 	ret	
 
-
 ma_toa:
 	push	rdx
 	or	rdx, rdx
 	jz	.rt
-
+	mov	 r8, '89abcdef'		; lookup table, little endian
 .l0:	mov	rcx, rdx		; rotate rsi, each digit is 4 bits
 	shl	 cl, 2
 	ror	rsi, cl
-	cld
 .l1:	rol	rsi, 4
 	mov	rcx, rsi
-	and	 cl,  0x0f		; extract and clear this digit, so it
-	and	rsi, -0x10		; won't reappear after full lap of rol's
+	and	 cl,  0x0f		; extract and clear digit, so it won't
+	and	rsi, -0x10		;   reappear after full lap of rol's
 	mov	rax, '01234567'
-	cmp	 cl, 8
-	jl	.l2
-	mov	rax, '89abcdef'		; if the digit is greater than 8,
-	sub	 cl, 8			;   we use 2nd half of the lookup table
-.l2:	shl	 cl, 3			; each ascii sign is 8 bits
+	cmp	 cl, 8			; if the digit is greater than 8,
+	cmovge	rax, r8			;   we use 2nd half of the lookup table
+	and	 cl, 0xf7		;   and subtract 8
+	shl	 cl, 3			; each ascii sign is 8 bits
 	shr	rax, cl
 	stosb
 	dec	rdx
 	jnz	.l1
-
 .rt:	pop	rax
 	ret
